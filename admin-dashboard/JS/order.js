7e8e0bb0d3c89ecfc16f346e82b61ba2283e688a
TushaR <104409472+Sainitu@users.noreply.github.com>
2023-02-21 10:59:00 +0530

    admin panel
    Sidebar updation

---
+++ admin-dashboard/JS/order.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Instagram',
+        productNumber: '85631',
+        paymentStatus: 'Social Media Platform Company',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Cristiano Ronaldo',
+        productNumber: '36378',
+        paymentStatus: 'Footballer',
+        shipping: 'NOT ACTIVE',
+    },
+    {
+        productName: 'Lionel Messi',
+        productNumber: '49347',
+        paymentStatus: 'Footballer',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Kylie Jenner',
+        productNumber: '96996',
+        paymentStatus: 'Television Personality / Model ',
+        shipping: 'Active',
+    },
+    {
+        productName: 'Selena Gomez',
+        productNumber: '22821',
+        paymentStatus: 'Musician / Actress',
+        shipping: 'Active',
+    },
+    {
+        productName: 'Dwayne Johnson',
+        productNumber: '81475',
+        paymentStatus: 'Actor/ Professional Wrestler',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Justin Bieber',
+        productNumber: '10482',
+        paymentStatus: 'Musician',
+        shipping: 'Active',
+    }
+]
